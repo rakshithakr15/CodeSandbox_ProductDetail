892016047f2f4e76abb00dfbc734081758a2f8ef
vratnesh <vratnesh@in.ibm.com>
2021-05-17 00:39:17 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductsService } from "./products.service";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  declarations: [AppComponent, ProductDetailComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [ProductsService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
